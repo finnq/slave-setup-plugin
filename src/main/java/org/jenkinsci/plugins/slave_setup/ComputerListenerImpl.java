61080b85e5b993bda51896008471f3cab4caf8a1
Hendrik H <finnq@finnq.de>
2013-08-02 20:26:42 +0200

    Update ComputerListenerImpl.java

--- src/main/java/org/jenkinsci/plugins/slave_setup/ComputerListenerImpl.java
+++ src/main/java/org/jenkinsci/plugins/slave_setup/ComputerListenerImpl.java
@@ -1,11 +1,9 @@
 package org.jenkinsci.plugins.slave_setup;
 
-import hudson.Extension;
-import hudson.FilePath;
 import hudson.model.Computer;
 import hudson.model.TaskListener;
-import hudson.remoting.Channel;
 import hudson.slaves.ComputerListener;
+import hudson.AbortException;
 
 import java.io.IOException;
 import java.util.List;
@@ -19,14 +17,12 @@ public class ComputerListenerImpl extends ComputerListener {
     /**
      * Prepares the slave before it gets online by copying the given content in root and executing the configured setup script.
      * @param c the computer to set up
-     * @param channel not used
-     * @param root the root of the slave
      * @param listener log listener
      * @throws IOException
      * @throws InterruptedException
      */
     @Override
-    public void preOnline(Computer c, Channel channel, TaskListener listener) throws IOException, InterruptedException {
+    public void preOnline(Computer c, TaskListener listener) throws AbortException, IOException, InterruptedException {
         listener.getLogger().println("just before slave " + c.getName() + " gets online ...");
 
         SetupConfig config = SetupConfig.get();
